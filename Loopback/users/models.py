9aac49a2275aefcf0b844bfe59baf02a5836f8e9
iamdamified <119352851+iamdamified@users.noreply.github.com>
2025-04-14 13:40:38 +0100

    Abstractuser, Profile, Basic tools, and Database setup

---
+++ Loopback/users/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('mentor', 'Mentor'),
+        ('mentor', 'Mentor')
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.username} {self.role}"
+    
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    interests = models.CharField(max_length=255, blank=True, null=True)
+    goals = models.CharField(max_length=255, blank=True, null=True)
+    skills = models.CharField(max_length=255, blank=True, null=True)
+    experience = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
